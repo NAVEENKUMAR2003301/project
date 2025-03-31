a1ae5d76b7a2bc02dbf8470ce0e31cee1e1a99c6
NAVEENKUMAR V <livnaveen@gmail.com>
2025-03-31 08:15:11 +0530

    media query complete

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const search=document.querySelector(".search-inp")
 const navLinks=document.querySelector(".navbar>.nav-links")
+const sidebar=document.querySelector(".sidebar")
 
 function searchFunc(){
     search.classList.toggle("hidded")
     navLinks.classList.toggle("hidded")
+}
+
+function sideBarOpen(){
+    sidebar.style.right="0"
+}
+
+function sideBarClose(){
+    sidebar.style.right="-200%"
 }
