4322efb37c216e1a668f993a6c6e143533e4b2aa
NAVEENKUMAR V <livnaveen@gmail.com>
2025-03-31 09:36:09 +0530

    js funcs

--- app.js
+++ app.js
@@ -1,17 +1,78 @@
-const search=document.querySelector(".search-inp")
-const navLinks=document.querySelector(".navbar>.nav-links")
-const sidebar=document.querySelector(".sidebar")
+const search = document.querySelector(".search-inp")
+const navLinks = document.querySelector(".navbar>.nav-links")
+const sidebar = document.querySelector(".sidebar")
 
-function searchFunc(){
+function searchFunc() {
     search.classList.toggle("hidded")
     navLinks.classList.toggle("hidded")
 }
 
-function sideBarOpen(){
-    sidebar.style.right="0"
+function sideBarOpen() {
+    sidebar.style.right = "0"
 }
 
-function sideBarClose(){
-    sidebar.style.right="-200%"
+function sideBarClose() {
+    sidebar.style.right = "-200%"
+}
+
+const tests = document.querySelectorAll(".section10>.test-cont>.test");
+const dot=document.querySelector(".section10>.slide>.dot")
+
+let currentTest = 0;
+
+function prevTest() {
+
+    if (currentTest === 0) {
+        currentTest = 2
+    } else {
+        currentTest--
+    }
+    tests[currentTest].classList.add("forword")
+    tests[currentTest].classList.remove("reverse")
+    tests.forEach((ele, ind) => {
+        if (ind !== currentTest) {
+            ele.classList.add("hidd")
+            ele.classList.remove("forword")
+            ele.classList.remove("reverse")
+        }else{
+            ele.classList.remove("hidd")
+        }
+    })
+
+    if(currentTest==0){
+        dot.style.order="0"
+    }else if(currentTest==1){
+        dot.style.order="2"
+    }else{
+        dot.style.order="4"
+    }
+
+}
+function nextTest() {
+
+    if (currentTest === 2) {
+        currentTest = 0
+    } else {
+        currentTest++
+    }
+    tests[currentTest].classList.add("reverse")
+    tests[currentTest].classList.remove("forword")
+    tests.forEach((ele, ind) => {
+        if (ind !== currentTest) {
+            ele.classList.add("hidd")
+            ele.classList.remove("forword")
+            ele.classList.remove("reverse")
+        }else{
+            ele.classList.remove("hidd")
+        }
+    })
+
+    if(currentTest==0){
+        dot.style.order="0"
+    }else if(currentTest==1){
+        dot.style.order="2"
+    }else{
+        dot.style.order="4"
+    }
 }
 
